2404dfe6b69d5770ca1815cd667dc65b23aeef1b
enigma1940 <sam.corp@outlook.com>
2017-06-14 17:33:54 +0000

    Working on a new interface

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+  include_once('php/connectdb.php');
+?>
 <!DOCTYPE html>
 <html>
   <head>
@@ -12,6 +15,77 @@
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
   </head>
   <body>
+    <header class="row">
+      <div class="col m1 logo"><img src="fonts/logo.png" style="width: 70%;" /></div>
+      <div class="col m8 title">
+        BDNE :: BANQUE NATIONALE DE DONNEES D'EXPERTISE
+      </div>
+      <div class="col m1 item waves-effect waves-yellow">Accueil</div>
+      <div class="col m1 item waves-effect waves-yellow">Connexion</div>
+      <div class="col m1 item waves-effect waves-yellow">A propos</div>
+    </header>
+    <div class="row mainContainer">
+      <div class="col m12 searchArea">
+        <form class="searchForm col m12">
+          <div class="col m1 captionSearch">Rechercher : </div>
+          <div class="input-field col m5" style="margin: 0px;">
+            <i class="material-icons prefix">search</i>
+            <input type="text" class="searchText" />
+            <label>Faire une recherche</label>
+          </div>
+          <select class="col m2 univChoice">
+            <option value="">Choisir une université?</option>
+            <?php
+              $r=$bdd->query('SELECT * FROM universite');
+              while($d=$r->fetch()){
+                echo '<option value="'.$d['code'].'">'.$d['nom'].'</option>"';
+              }
+            ?>
+          </select>
+          <select class="col m2 univChoice">
+            <option value="">Choisir une filière?</option>
+            <?php
+              $r=$bdd->query('SELECT * FROM filiere');
+              while($d=$r->fetch()){
+                echo '<option value="'.$d['code'].'">'.$d['nom'].'</option>"';
+              }
+            ?>
+          </select>
+          <select class="col m1 univChoice">
+            <option value="">Année?</option>
+            <?php
+              $r=$bdd->query('SELECT DISTINCT annee FROM document ORDER BY annee DESC');
+              while($d=$r->fetch()){
+                echo '<option>'.$d['annee'].'</option>';
+              }
+            ?>
+          </select>
+          <div class="col m1">
+            <button type="submit" class="btn waves-effect waves-light"><i class="material-icons">search</i></button>
+          </div>
+        </form>
+      </div>
+
+      <div class="col m12 rubriqueArea">
+        <?php
+          $r=$bdd->query('SELECT id ,code, nom FROM filiere');
+          while($d=$r->fetch()){
+            echo '<div class="col m2">
+              <div class="col m12">'.$d['code'].'</div>
+              <div class="col m12">'.$d['nom'].'</div>';
+              $r0 =$bdd->prepare('SELECT count(*) as n FROM document WHERE filiere_id=?');
+              $r0->execute(array($d['id']));
+              $d0=$r0->fetch();
+              echo '<div class="col m12">'.$d0['n'].' items</div>';
+            echo '</div>';
+          }
+        ?>
+      </div>
+
+
+
+    </div>
+
     <script src="js/home.js"></script>
   </body>
 </html>
---
+++ js/home.js
@@ -0,0 +1,4 @@
+$('.title').css('line-height', $('.logo').css('height')); $('.item').css('line-height', $('.logo').css('height'));
+//$('.mainContainer').css('margin-top: ');
+$('select').material_select();
+$('.captionSearch').css('line-height', $('.searchText').css('height'));
